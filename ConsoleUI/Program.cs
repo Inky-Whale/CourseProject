69a2138edbfd7da69fb7108ae39adfb8989302a8
Inky-Whale <berk.karanfiloglu@gmail.com>
2023-12-27 22:47:37 +0300

    Add project files.

---
+++ ConsoleUI/Program.cs
@@ -0,0 +1,23 @@
+﻿using Business.Concrete;
+using DataAccess.Concrete.EntityFramework;
+using DataAccess.Concrete.InMemory;
+
+namespace ConsoleUI
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            //ProductManager productManager = new ProductManager(new InMemoryProductDal());
+            ProductManager productManager2 = new ProductManager(new EfProductDal());
+            
+
+            
+
+            foreach (var product in productManager2.GetByUnitPrice(5,15))
+            {
+                Console.WriteLine(product.ProductName + "----" + product.UnitPrice);
+            }
+        }
+    }
+}
